29c8b7a2e12a0717c0741dd9916798cb6311b3fe
Naveenkumar1901 <mpsd19@gmail.com>
2022-09-19 00:06:28 +0530

    env variables added

--- src/Firebase-config.js
+++ src/Firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import {} from "dotenv/config";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
-  projectId: process.env.REACT_APP_FIREBASE_API_KEY,
-  storageBucket: process.env.REACT_APP_FIREBASE_API_KEY,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_API_KEY,
-  appId: process.env.REACT_APP_FIREBASE_API_KEY,
-  measurementId: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
